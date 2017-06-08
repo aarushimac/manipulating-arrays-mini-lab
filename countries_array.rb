eb79287cb4f875a086b8309ee3932c644543eed9
Aarushi Machavarapu <aarushimac@gmail.com>
2017-06-08 14:33:16 +0000

    countries array lab final

--- countries_array.rb
+++ countries_array.rb
@@ -1,1 +1,15 @@
+four_letter_countries=["Chad", "Cuba", "Greenland", "iraq", "mali", "oman", "india"]
 
+four_letter_countries.push("fiji")
+four_letter_countries.insert(4,"iran")
+four_letter_countries.delete("india")
+four_letter_countries = four_letter_countries.reverse
+
+
+four_letter_countries[5] = "Togo"
+four_letter_countries.push("laos")
+four_letter_countries = four_letter_countries.reverse
+four_letter_countries.insert(0, "peru")
+
+
+puts four_letter_countries
